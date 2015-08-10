a5ca4bbc0e3211d52fa89e2d8daa9553b5fd241e
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2015-08-10 22:05:42 +0900

    add stub

---
+++ src/main/java/loglang/Main.java
@@ -0,0 +1,10 @@
+package loglang;
+
+/**
+ * Created by skgchxngsxyz-osx on 15/08/10.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/loglang/MainTest.java
@@ -0,0 +1,10 @@
+package loglang;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by skgchxngsxyz-osx on 15/08/10.
+ */
+public class MainTest {
+
+}
