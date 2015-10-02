6c70f0a1cafc844072979fcd63f8f086ed641a2b
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2015-10-02 12:16:25 +0900

    add time option

--- src/main/java/loglang/Main.java
+++ src/main/java/loglang/Main.java
@@ -9,19 +9,23 @@ import loglang.misc.ArgsParser;
 public class Main {
     private static String scriptFileName = null;
     private static String inputFileName = null;
+    private static boolean time = false;
 
     public static void main(String[] args) {
         ArgsParser parser = new ArgsParser();
 
         parser.addOption("s", "script", HAS_ARG | REQUIRE,
                 "specify script file name",
-                (a) -> scriptFileName = a.get());
+                a -> scriptFileName = a.get());
         parser.addOption("i", "input", HAS_ARG | REQUIRE,
                 "specify intput file name",
-                (a) -> inputFileName = a.get());
+                a -> inputFileName = a.get());
         parser.addOption("h", "help", IGNORE_REST,
                 "show this help message",
-                (a) -> { parser.printHelp(System.out); System.exit(0); });
+                a -> { parser.printHelp(System.out); System.exit(0); });
+        parser.addOption("t", "time", 0,
+                "show execution time",
+                a -> time = true);
 
         try {
             parser.parse(args);
@@ -32,6 +36,17 @@ public class Main {
         }
 
         Loglang ll = new LoglangFactory().newLoglang(scriptFileName);
+        if(time) {
+            time(ll);
+        } else {
+            ll.invoke(inputFileName);
+        }
+    }
+
+    private static void time(Loglang ll) {
+        long start = System.currentTimeMillis();
         ll.invoke(inputFileName);
+        long stop = System.currentTimeMillis();
+        System.out.println("execution time: " + (stop - start) + "ms");
     }
 }
